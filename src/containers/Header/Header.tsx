4b2dcc27d0be307b87457d244b2bdce702fd2cba
frostwOw13 <frostwow13@yandex.ru>
2021-08-06 17:10:48 +0300

    feat: add fetching data from api

--- src/containers/Header/Header.tsx
+++ src/containers/Header/Header.tsx
@@ -1,20 +1,67 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './Header.scss';
 
 const Header: React.FC = () => {
+  const [searchValue, setSearchValue] = useState<string>('');
+  const [orderBy, setOrderBy] = useState<string>('relevance');
+  const [category, setCategory] = useState<string>('all');
+
+  const getBooks = async () => {
+    const response = await fetch(
+      `https://www.googleapis.com/books/v1/volumes?q=${searchValue}:${category === 'all' ? '' : category}&maxResults=30&orderBy=${orderBy}`,
+    );
+    const data = await response.json();
+    console.log(data);
+  };
+
+  const submitHandler = async (event: any) => {
+    event.preventDefault();
+    setSearchValue('');
+    getBooks();
+  };
+
   return (
     <header className="header">
       <h1 className="title">Search for books</h1>
-      <input type="text" className="search-bar" placeholder="Search..." />
-      <div className="header__options">
-        <select className="category">
-          <option>all</option>
-        </select>
-        <select className="sortby">
-          <option>relevance</option>
-          <option>newest</option>
-        </select>
-      </div>
+      <form className="form" onSubmit={submitHandler}>
+        <div className="form__search">
+          <input
+            type="text"
+            className="search-bar"
+            placeholder="Search..."
+            value={searchValue}
+            onChange={(event) => {
+              setSearchValue(event.target.value);
+            }}
+          />
+          <button className="icon" type="submit">
+            <div className="icon__image" />
+          </button>
+        </div>
+
+        <div className="form__options">
+          <select
+            className="category"
+            onChange={(event) => {
+              setCategory(event.target.value);
+            }}
+          >
+            <option>all</option>
+            <option>fiction</option>
+            <option>fantasy</option>
+            <option>computer</option>
+          </select>
+          <select
+            className="sortby"
+            onChange={(event) => {
+              setOrderBy(event.target.value);
+            }}
+          >
+            <option>relevance</option>
+            <option>newest</option>
+          </select>
+        </div>
+      </form>
     </header>
   );
 };
